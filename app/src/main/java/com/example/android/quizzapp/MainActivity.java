a417858aac04cb7cdee61e372fa813c46d613ac1
Meliphant <op.personal.analytics@gmail.com>
2017-12-30 18:04:30 +0300

    New features added
    Added auto scroll to the bottom of the screen, changed onBackPressed method.

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.android.quizzapp;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 
@@ -41,38 +44,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
     @Override
     public void onBackPressed() {
-        AlertDialog.Builder exit = new AlertDialog.Builder(MainActivity.this);
-        exit
+        new AlertDialog.Builder(this)
                 .setMessage(R.string.exitApp)
-                .setNegativeButton(R.string.yes,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                exit();
-                            }
-                        })
-                .setPositiveButton(R.string.no,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                return;
-                            }
-                        });
-
-        AlertDialog alertDialog_exit = exit.create();
-        alertDialog_exit.show();
-        Button p = alertDialog_exit.getButton(DialogInterface.BUTTON_POSITIVE);
-        p.setTextColor(Color.BLACK);
-        Button n = alertDialog_exit.getButton(DialogInterface.BUTTON_NEGATIVE);
-        n.setTextColor(Color.BLACK);
-    }
-
-    public void exit() {
-        Intent a = new Intent(Intent.ACTION_MAIN);
-        a.addCategory(Intent.CATEGORY_HOME);
-        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(a);
+                .setNegativeButton(R.string.no, null)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        MainActivity.super.onBackPressed();
+                    }
+                }).create().show();
     }
 }
--- app/src/main/java/com/example/android/quizzapp/QuizActivity.java
+++ app/src/main/java/com/example/android/quizzapp/QuizActivity.java
@@ -1,27 +1,29 @@
 package com.example.android.quizzapp;
 
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.content.res.Resources;
-        import android.graphics.Color;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.support.v7.app.AlertDialog;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.CheckBox;
-        import android.widget.ImageView;
-        import android.widget.RadioButton;
-        import android.widget.RadioGroup;
-        import android.widget.TextView;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button submit;
-    int score = 0, buttonState = 1;
+    private Button submit;
+    TextView scoreResult;
+    private int score = 0, buttonState = 1;
     String userName, message;
     ImageView picResult;
-
+    ScrollView getScrollView;
     /**
      * ID array for all RadioGroups
      */
@@ -62,13 +64,14 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
                     getRadioAnswers();
                     getCheckBoxAnswers();
                     checkAnswer();
+                    scrollDialogDown();
                 } else resetAnswer();
                 //Toast.makeText(getApplicationContext(), "Your score is "  + score, Toast.LENGTH_SHORT).show();
                 break;
         }
     }
     private void checkAnswer(){
-        TextView scoreResult = (TextView) findViewById(R.id.scoreResult);
+        scoreResult = (TextView) findViewById(R.id.scoreResult);
         picResult = (ImageView) findViewById(R.id.pic_answer);
         message = getTheMessage(score, picResult);
         scoreResult.setText(message);
@@ -82,8 +85,8 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     private void resetAnswer(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        TextView scoreResult = (TextView) findViewById(R.id.scoreResult);
-        ImageView picResult = (ImageView) findViewById(R.id.pic_answer);
+        scoreResult = (TextView) findViewById(R.id.scoreResult);
+        picResult = (ImageView) findViewById(R.id.pic_answer);
         scoreResult.setVisibility(View.GONE);
         picResult.setVisibility(View.GONE);
         submit.setText(getString(R.string.again));
@@ -138,55 +141,43 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    //set the result message after Submit button clicked
+    //set the result message after Submit button is clicked
     private String getTheMessage(int score, ImageView picResult) {
         Intent getQuizActivity = getIntent();
         userName = getQuizActivity.getStringExtra(MainActivity.EXTRA_MESSAGE);
         message = getString(R.string.dear) + userName + "!\n" + getString(R.string.messageBase) + score;
-       if (score > 6) {
-           message += getString(R.string.messageresultGenius);
-           picResult.setImageResource(R.drawable.answer1);
-       } else if (score > 3) {
-           message += getString(R.string.messageresultCaptain);
-           picResult.setImageResource(R.drawable.answer2);
-       } else {
-           message += getString(R.string.messageresultSlowpoke);
-           picResult.setImageResource(R.drawable.answer3);
-       }
+        if (score > 6) {
+            message += getString(R.string.messageresultGenius);
+            picResult.setImageResource(R.drawable.answer1);
+        } else if (score > 3) {
+            message += getString(R.string.messageresultCaptain);
+            picResult.setImageResource(R.drawable.answer2);
+        } else {
+            message += getString(R.string.messageresultSlowpoke);
+            picResult.setImageResource(R.drawable.answer3);
+        }
         return message;
     }
-    @Override
-    public void onBackPressed() {
-        AlertDialog.Builder exit = new AlertDialog.Builder(QuizActivity.this);
-        exit
-                .setMessage(R.string.exitApp)
-                .setNegativeButton(R.string.yes,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                exit();
-                            }
-                        })
-                .setPositiveButton(R.string.no,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                return;
-                            }
-                        });
 
-        AlertDialog alertDialog_exit = exit.create();
-        alertDialog_exit.show();
-        Button p = alertDialog_exit.getButton(DialogInterface.BUTTON_POSITIVE);
-        p.setTextColor(Color.BLACK);
-        Button n = alertDialog_exit.getButton(DialogInterface.BUTTON_NEGATIVE);
-        n.setTextColor(Color.BLACK);
+    private void scrollDialogDown() {
+        getScrollView = (ScrollView) findViewById(R.id.scrollview);
+        getScrollView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        }, 100);
     }
 
-    public void exit() {
-        Intent setMainActivity = new Intent(Intent.ACTION_MAIN);
-        setMainActivity.addCategory(Intent.CATEGORY_HOME);
-        setMainActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(setMainActivity);
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.exitQuiz)
+                .setNegativeButton(R.string.no, null)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        QuizActivity.super.onBackPressed();
+                    }
+                }).create().show();
     }
 }
