d3b0afdb4ff4d2122bed2d38490506e1570f928e
Andrey Radionov <andyomsk@gmail.com>
2017-12-30 22:56:03 +0300

    Code Review

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -23,25 +23,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //REVU Я без понятия что это. Чтото мягкое :)
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+        //REVU Студия говорит что кастинг не нужен
         Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        //REVU switch тут излишен и break тоже
         switch (view.getId()) {
             case R.id.startButton:
+                //REVU интент можно назвать типа startQuizActivityIntent
+                //Хотя я не знаю BestPractices на этот счет
                 Intent intent = new Intent(this, QuizActivity.class);
 
+                //REVU Студия говорит что кастинг не нужен
+                //Не знаю скажется ли на скорости, можно попробовать name вынести в поле класса
                 EditText name = (EditText) findViewById(R.id.name);
                 String userName = name.getText().toString();
+                //REVU Ктото говорит, что если в одну строку то скобки можно не ставить
+                //Ктото против. Я стараюсь всегда {} ставить
                 if (userName.isEmpty()) userName = getString(R.string.username);
 
                 intent.putExtra(EXTRA_MESSAGE, userName);
                 startActivity(intent);
                 break;
+            //REVU else { ErrorTextView.setText("Введите имя!")} и не стартуем активити
         }
     }
 
